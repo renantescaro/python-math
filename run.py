5b4197e8e66b469810a80a0f59f9758968915a42
renantescaro <renantescaro@yahoo.com.br>
2022-05-02 22:53:54 -0300

    adicionando geração de gráfico

--- run.py
+++ run.py
@@ -1,15 +1,24 @@
 from entradas import Entradas
 from equacoes.segundo_grau import SegundoGrau
+from grafico import Grafico
 
 
-print('Exemplo de entrada x²+5x+6 | x²+4x+4 | x²+6x+13')
+print('Exemplo de entrada x²+5x+6 | +x²+4x+4 | x²+6x+13')
 entrada_str = str(input('Digite: '))
 
-segundo_grau = SegundoGrau(
-    entradas=Entradas(entrada_str)
+entradas=Entradas(entrada_str)
+segundo_grau=SegundoGrau(
+    entradas=entradas
 )
-
 raiz_1, raiz_2, delta = segundo_grau.executar()
 print('raiz_1', raiz_1)
 print('raiz_2', raiz_2)
 print('delta', delta)
+
+grafico=Grafico(
+    a=entradas.a,
+    b=entradas.b,
+    c=entradas.c,
+    equacao=entradas.equacao
+)
+grafico.executar()
