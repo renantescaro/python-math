899c78e4ee126f63bc9e869eb5c866d179712811
buguno <brunnodesouzabezerra@gmail.com>
2022-05-04 19:02:11 -0300

    refactor: adding autopep8 style

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ from grafico import Grafico
 print('Exemplo de entrada x²+5x+6 | +x²+4x+4 | x²+6x+13')
 entrada_str = str(input('Digite: '))
 
-entradas=Entradas(entrada_str)
-segundo_grau=SegundoGrau(
+entradas = Entradas(entrada_str)
+segundo_grau = SegundoGrau(
     entradas=entradas
 )
 raiz_1, raiz_2, delta = segundo_grau.executar()
@@ -15,7 +15,7 @@ print('raiz_1', raiz_1)
 print('raiz_2', raiz_2)
 print('delta', delta)
 
-grafico=Grafico(
+grafico = Grafico(
     a=entradas.a,
     b=entradas.b,
     c=entradas.c,
